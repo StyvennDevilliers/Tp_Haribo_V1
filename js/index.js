806fa571c2b82f00bf5955ed96b27a316233f243
Styvenn <styvenn.devilliers@lyceeastier.com>
2026-02-26 11:55:42 +0100

    commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("test js")
+
+document.onreadystatechange = function () {
+    console.log(document.readyState)
+    if(document.readyState == 'interactive') {
+        console.log("c'est ok")
+
+    }
+}
