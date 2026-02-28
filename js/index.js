3f9a46014f16ba22c15f7265999db79e45ed2403
StyvennDevilliers <styvenn.devilliers@lyceeastier.com>
2026-02-28 13:46:18 +0100

    commit

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ document.onreadystatechange = function () {
     console.log(document.readyState)
     if(document.readyState == 'interactive') {
         console.log("c'est ok")
-        //TODO: add tagada, ajouter le path des images dans les objets et essayer de faire une boucle pour les infos de la commande 
+        //TODO add tagada, ajouter le path des images dans les objets et essayer de faire une boucle pour les infos de la commande
 
-        let prix_c = 2.28;
-        let prix_d = 1.64;
+        let prix_Chamallows = 2.28;
+        let prix_Dragibus = 1.64;
         let poid_init_c = 100;
         let poid_init_d = 300;
         let commande  = document.getElementById("commande-list");
@@ -24,16 +24,17 @@ document.onreadystatechange = function () {
         let moins_2 = document.getElementById("moins_2");
 
         const state = {
-            chamallows: {name:"Chamallows", prix: prix_c,  qty: parseInt(input_1.value), poids: poid_init_c },
-            dragibus:   {name:"Dragibus", prix: prix_d,  qty: parseInt(input_2.value), poids: poid_init_d }
+            chamallows: {name:"Chamallows", prix: prix_Chamallows,  qty: parseInt(input_1.value), poids: poid_init_c, img: "images/chamallows.jpg" },
+            dragibus:   {name:"Dragibus", prix: prix_Dragibus,  qty: parseInt(input_2.value), poids: poid_init_d, img: "images/dragibus.jpg" }
         };
 
-        document.getElementById("prix_c").innerText = "Prix à l'unité " + state.chamallows.prix + " €";
-        document.getElementById("poids_c").innerText = "Poids " + state.chamallows.poids + "g";
-        document.getElementById("name_1").innerText = state.chamallows.name;
-        document.getElementById("prix_d").innerText = "Prix à l'unité " + state.dragibus.prix + " €";
-        document.getElementById("poids_d").innerText = "Poids " + state.dragibus.poids + "g";
-        document.getElementById("name_2").innerText = state.dragibus.name;
+        Object.values(state).forEach(object => {
+            document.getElementById("prix_" + object.name).innerText = "Prix à l'unité " + object.prix + " €";
+            document.getElementById("poids_" + object.name).innerText = "Poids " + object.poids + "g";
+            document.getElementById("name_" + object.name).innerText = object.name;
+            document.getElementById("img_" + object.name).src = object.img;
+        })
+
         refreshDragibus()
         refreshChamallows()
 
@@ -107,14 +108,30 @@ document.onreadystatechange = function () {
         function refreshDragibus(){
             document.getElementById("nbr_d").innerText = state.dragibus.qty + " paquets de Dragibus"
             document.getElementById("prix_tot_d").innerText ="Prix total = " + (state.dragibus.prix * state.dragibus.qty).toFixed(2) + " €";
-            document.getElementById("poids_tot_d").innerText ="Poids total = " + state.dragibus.poids*state.dragibus.qty + "g";
-            document.getElementById("prix_kilo_d").innerText ="Prix au kilo = " +((state.dragibus.poids*state.dragibus.qty)/(state.dragibus.prix * state.dragibus.qty)).toFixed(2) + " €";
+            let poid = state.dragibus.poids*state.dragibus.qty;
+            if(poid>=1000) {
+                unité = "kg";
+                poid = poid/1000;
+            }
+            else{
+                unité = "g";
+            }
+            document.getElementById("poids_tot_d").innerText ="Poids total = " + poid + unité;
+            document.getElementById("prix_kilo_d").innerText ="Prix au kilo = " +((state.dragibus.prix*1000)/ state.dragibus.poids).toFixed(2) + " €";
         }
         function refreshChamallows(){
             document.getElementById("nbr_c").innerText = state.chamallows.qty + " paquets de Chamallows"
             document.getElementById("prix_tot_c").innerText ="Prix total = " + (state.chamallows.prix * state.chamallows.qty).toFixed(2) + " €";
-            document.getElementById("poids_tot_c").innerText ="Poids total = " + state.chamallows.poids*state.chamallows.qty + "g";
-            document.getElementById("prix_kilo_c").innerText ="Prix au kilo = " +((state.chamallows.poids*state.chamallows.qty)/(state.chamallows.prix * state.chamallows.qty)).toFixed(2) + " €";
+            let poid = state.chamallows.poids*state.chamallows.qty;
+            if(poid>=1000) {
+                unité = "kg";
+                poid = poid/1000;
+            }
+            else{
+                unité = "g";
+            }
+            document.getElementById("poids_tot_c").innerText ="Poids total = " + poid + unité;
+            document.getElementById("prix_kilo_c").innerText ="Prix au kilo = " +((state.chamallows.prix*1000)/state.chamallows.poids).toFixed(2) + " €";
         }
         function updateCommande(){
             refreshChamallows()
@@ -126,6 +143,18 @@ document.onreadystatechange = function () {
                 prix_total.innerHTML = "";
                 return;
             }*/
+            /*if(q1>0){
+                let li = document.createElement("li");
+                li.textContent = "Chamallows x " + q1 + " = " + (q1 * prix_Chamallows).toFixed(2) + " €";
+                commande.appendChild(li);
+                somme += q1 * prix_Chamallows;
+            }*/
+            /*if(q2>0){
+    let li = document.createElement("li");
+    li.textContent = "Dragibus x " + q2 + " = " + (q2 * prix_Dragibus).toFixed(2) + " €";
+    commande.appendChild(li);
+    somme += q2 * prix_Dragibus;
+}*/
             if(state.chamallows.qty <= 0 && state.dragibus.qty <= 0){
                 commande.innerHTML = "Aucun Article Sélectionné";
                 prix_total.innerHTML = "";
@@ -133,67 +162,55 @@ document.onreadystatechange = function () {
             }
 
             let somme = 0;
-            /*if(q1>0){
-                let li = document.createElement("li");
-                li.textContent = "Chamallows x " + q1 + " = " + (q1 * prix_c).toFixed(2) + " €";
-                commande.appendChild(li);
-                somme += q1 * prix_c;
-            }*/
-            if(state.chamallows.qty>0){
-                let li_name = document.createElement("li");
-                li_name.textContent = state.chamallows.name;
-                li_name.style.marginBottom = "10px";
-                li_name.style.fontSize = "20px";
-                commande.appendChild(li_name);
-                let li_nbr = document.createElement("li");
-                li_nbr.textContent = document.getElementById("nbr_c").innerText
-                li_nbr.style.marginBottom = "10px";
-                commande.appendChild(li_nbr);
-                let li_prix = document.createElement("li");
-                li_prix.textContent = document.getElementById("prix_tot_c").innerText
-                li_prix.style.marginBottom = "10px";
-                commande.appendChild(li_prix);
-                let li_poids = document.createElement("li");
-                li_poids.textContent = document.getElementById("poids_tot_c").innerText
-                li_poids.style.marginBottom = "20px";
-                commande.appendChild(li_poids);
-                somme += state.chamallows.qty * state.chamallows.prix;
-                commande.style.textAlign = "center";
+            Object.values(state).forEach(product => {
+                if(product.qty>0) {
+                    let li_name = document.createElement("li");
+                    li_name.textContent = product.name;
+                    li_name.style.marginBottom = "10px";
+                    li_name.style.fontSize = "20px";
+                    commande.appendChild(li_name);
+                    let li_nbr = document.createElement("li");
+                    li_nbr.textContent = state.chamallows.qty + " paquets de " + product.name;
+                    li_nbr.style.marginBottom = "10px";
+                    commande.appendChild(li_nbr);
+                    let li_prix = document.createElement("li");
+                    li_prix.textContent = "Prix total = " + (product.prix*product.qty).toFixed(2) + " €";
+                    li_prix.style.marginBottom = "10px";
+                    commande.appendChild(li_prix);
+                    let li_poids = document.createElement("li");
+                    let poid =product.poids*product.qty;
+                    let unité;
+                    if(poid>=1000) {
+                        unité = "kg";
+                        poid = poid/1000;
+                    }
+                    else{
+                        unité = "g";
+                    }
+                    li_poids.textContent = "Poids total = " + poid + unité;
+                    li_poids.style.marginBottom = "20px";
+                    commande.appendChild(li_poids);
+                    somme += product.qty * product.prix;
+                    commande.style.textAlign = "center";
+                }
+            });
 
+            let poids_tot = 0 ;
+            Object.values(state).forEach(product => {
+                poids_tot = poids_tot + (product.poids*product.qty);
+            })
+            if(poids_tot>=1000) {
+                unité = "kg";
+                poids_tot = poids_tot/1000;
             }
-
-            /*if(q2>0){
-                let li = document.createElement("li");
-                li.textContent = "Dragibus x " + q2 + " = " + (q2 * prix_d).toFixed(2) + " €";
-                commande.appendChild(li);
-                somme += q2 * prix_d;
-            }*/
-            if(state.dragibus.qty>0){
-                let li_name = document.createElement("li");
-                li_name.textContent = state.dragibus.name;
-                li_name.style.marginBottom = "10px";
-                li_name.style.fontSize = "20px";
-                commande.appendChild(li_name);
-                let li_nbr = document.createElement("li");
-                li_nbr.textContent = document.getElementById("nbr_d").innerText
-                li_nbr.style.marginBottom = "10px";
-                commande.appendChild(li_nbr);
-                let li_prix = document.createElement("li");
-                li_prix.textContent = document.getElementById("prix_tot_d").innerText
-                li_prix.style.marginBottom = "10px";
-                commande.appendChild(li_prix);
-                let li_poids = document.createElement("li");
-                li_poids.textContent = document.getElementById("poids_tot_d").innerText
-                li_poids.style.marginBottom = "20px";
-                commande.appendChild(li_poids);
-                commande.style.textAlign = "center";
-                somme += state.dragibus.qty * state.dragibus.prix;
+            else{
+                unité = "g";
             }
             commande.style.marginBottom = "20px";
             commande.append("Récapitulatif de la commande")
             prix_total.innerHTML ="Prix total = " + somme.toFixed(2) + " €";
             prix_total.style.textAlign = "center";
-            poids_total.innerHTML ="Poids total = " + (state.chamallows.poids*state.chamallows.qty+state.dragibus.poids*state.dragibus.qty) + "g";
+            poids_total.innerHTML ="Poids total = " + (poids_tot) + unité;
             poids_total.style.textAlign = "center";
         }
     }
